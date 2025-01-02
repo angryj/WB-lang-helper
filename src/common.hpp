496961122ee3be12b7bd855a7935172b92fc6920
Jason Wu <jasonxhwu@gmail.com>
2025-01-01 19:32:13 -0700

    improved header file

--- src/common.hpp
+++ src/common.hpp
@@ -7,5 +7,16 @@
 #define WORDS_AMOUNT 100
 #define FILTER_DUPLICATES
 
-std::unordered_map<std::string, std::vector<std::string>> main_dict;
-std::string order = "CV(C)";
+struct order_states {
+    bool parenthesis_open = false;
+    bool bracket_open = false;
+};
+
+// default values
+std::unordered_map<std::string, std::vector<std::string>> main_dict = {
+    {"C", {"p", "t", "k", "s", "m", "n"}}, // consonants, specifically stops
+    {"V", {"a", "i", "u"}}, // vowels
+    {"N", {"m", "n"}} // consonants, specifically nasals
+};
+
+std::string order = "CV(CV)(N)"; // default value
